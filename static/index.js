fcd8c4b67d8e8bf45ec31926fbbcb9ddcdc66090
glayn2bukman <glayn2bukman@gmail.com>
2019-02-19 17:33:11 +0300

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -1,6 +1,8 @@
 var SERVER = {protocol:'http://', ip:'0.0.0.0', port:8790};
 //var SERVER = {protocol:'', ip:'', port:''};
 
+var DEVICE_SERIAL = ''
+
 function _done_request(){
     document.getElementById('loading').style.display = 'none';
 
@@ -39,44 +41,31 @@ function request(url,method,payload,onsucess,onfailure,logtext="",glue=null, onp
 }
 
 function readserial(){
-    document.getElementById('serial').innerHTML = device.serial;
+    DEVICE_SERIAL = device.serial;
 }
 
 function readbarcode(){
-    try{
-        var bcs = cordova.plugins.barcodeScanner.scan;
-    }catch(e){
-        flag_error('aint no barcode scanner here!');
-        return;
+}
+
+function back(div){
+    if(div=='personnel'){
+        document.getElementById('personnel').style.display='none';
+        document.getElementById('inspection').style.display='block';
+    }else if(div=='inspection'){
+        document.getElementById('inspection').style.display='none';
+        document.getElementById('meter_details').style.display='block';
+    }else if(div=='meter_details'){        
     }
-    
-    cordova.plugins.barcodeScanner.scan(
-      function (result) {
-            document.getElementById('barcodecode').innerHTML = result.text+':::'+result.format+':::'+result.cancelled;
-/*          
-            alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-*/
-      },
-      function (error) {
-          document.getElementById('barcodecode').innerHTML = "Scanning failed: " + error;
-      },
-      {
-          preferFrontCamera : true, // iOS and Android
-          showFlipCameraButton : true, // iOS and Android
-          showTorchButton : true, // iOS and Android
-          torchOn: true, // Android, launch with the torch switched on (if available)
-          saveHistory: true, // Android, save scan history (default false)
-          prompt : "Place a barcode inside the scan area", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS and Android
-      }
-   );
+}
+function next(div){
+    if(div=='personnel'){
+    }else if(div=='inspection'){
+        document.getElementById('inspection').style.display='none';
+        document.getElementById('personnel').style.display='block';
+    }else if(div=='meter_details'){        
+        document.getElementById('meter_details').style.display='none';
+        document.getElementById('inspection').style.display='block';
+    }
 }
 
 document.addEventListener("deviceready", function(){
