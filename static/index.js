d69c040fcd1d0c7a43b200ddab3fe7e4d6999447
glayn2bukman <glayn2bukman@gmail.com>
2019-02-20 17:09:24 +0300

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -85,7 +85,8 @@ function back(div){
     }else if(div=='inspection'){
         document.getElementById('inspection').style.display='none';
         document.getElementById('meter_details').style.display='block';
-    }else if(div=='meter_details'){        
+    }else if(div=='meter_details'){
+        logout();       
     }
 }
 function next(div){
@@ -100,6 +101,17 @@ function next(div){
 }
 
 function get_location(){
+    /*
+        in the config.xml, add
+        
+        <plugin name="cordova-plugin-geolocation" version="2.1.0" />
+
+        and NOT just
+
+        <plugin name="cordova-plugin-geolocation" />
+
+    */
+
     try{
         navigator.geolocation.getCurrentPosition(
             function(pos){
@@ -137,6 +149,77 @@ function login(){
 
 }
 
+var _swipe = {startX:0,startY:0};
+
+function initSwipe(element,callback,threshold=20, other=null){
+    /*
+        callback will be given one argument, swap_data in the form of
+            {
+                horizontal: "none|left|right",
+                vertical  : "none|up|down",
+                resultant : "none|left|right|up|down"
+            }
+        
+        other: this data will be passed along to the callback
+
+    */
+    function _get_swipe_directions(dx,dy,threshold){
+        dx = dx<0?((dx>-threshold)?0:dx):((dx<threshold)?0:dx);
+        dy = dy<0?((dy>-threshold)?0:dx):((dy<threshold)?0:dy);
+        
+        var vertical_swipe = (dy>0)?"down":(dy<0?"up":"none");
+        var horizontal_swipe = (dx>0)?"right":(dx<0?"left":"none");
+
+        dy = dy<0?-1*dy:dy;
+        dx = dx<0?-1*dx:dx;
+
+        var direction = dy>dx?vertical_swipe:horizontal_swipe;
+
+        var swipe_data = {horizontal:horizontal_swipe,vertical:vertical_swipe,resultant:direction}
+        
+        return swipe_data;
+        
+    }
+
+    // mobile with touch events
+    element.addEventListener("touchstart",function(e){
+        e.stopPropagation();
+        _swipe.startX=e.changedTouches[0].pageX; _swipe.startY=e.changedTouches[0].pageY;});
+    element.addEventListener("touchend",function(e){
+        e.stopPropagation();
+        var dx = e.changedTouches[0].pageX-_swipe.startX, dy = e.changedTouches[0].pageY-_swipe.startY;
+        callback(_get_swipe_directions(dx,dy,threshold), other);
+    });
+
+    // PC with mouse events...
+    element.addEventListener("mousedown",function(e){
+        e.stopPropagation();
+        _swipe.startX=e.clientX; _swipe.startY=e.clientY;});
+    element.addEventListener("mouseup",function(e){
+        e.stopPropagation();
+        var dx = e.clientX-_swipe.startX, dy = e.clientY-_swipe.startY
+        callback(_get_swipe_directions(dx,dy,threshold), other);
+    });
+    
+}
+
+
 document.addEventListener("deviceready", function(){
     readserial();
 }, false);
+
+window.onload = function(){
+    
+    // to bend text...include the CirleType.min.js file
+    new CircleType(document.getElementById('title')).radius(190)/*.dir(-1)//this would reverse the bend*/;
+
+    let pages=['personnel','inspection','meter_details'];        
+
+    for(let i=0; i<pages.length; ++i){
+        initSwipe(document.getElementById(pages[i]), function(swipe_data,div_id){
+            if(swipe_data.resultant=="right"){back(div_id);}
+            else if(swipe_data.resultant=="left"){next(div_id);}
+        },200,other=pages[i]);        
+    }
+    
+}
